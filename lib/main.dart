fb860c53a55dee2290edf0cbc3b2f909fc0e353b
Adam Hammer <adamhammer@gmail.com>
2020-02-17 19:28:52 -0800

    update political correctness of library

--- lib/main.dart
+++ lib/main.dart
@@ -28,27 +28,51 @@ class FormTestApp extends StatelessWidget {
 /// This is the Sample Form used on the main() page
 const testFormFields = <FieldSpec>[
   FieldSpec(
-      name: "Name", mandatory: true, validators: [lengthValidator]),
+      name: "Name", 
+      mandatory: true, 
+      validators: [lengthValidator]),
+
   FieldSpec(
-      name: "Title", mandatory: false),
-  FieldSpec(name: "Age", mandatory: true, validators: [intValidator]),
+      name: "Title", 
+      mandatory: false),
+
+  FieldSpec(
+      name: "Email", 
+      mandatory: false, 
+      validators:[emailValidator]),
+
+  FieldSpec(
+      name: "Url", 
+      mandatory: false, 
+      validators: [urlValidator]),
+
+  FieldSpec(
+    name: "Age", 
+    mandatory: true, 
+    validators: [intValidator]),
+
   FieldSpec(
       name: "radio1",
       group: "Pronoun",
       value: "He",
       type: FieldType.radio),
+
   FieldSpec(
       name: "radio2",
       group: "Pronoun",
       value: "She",
       type: FieldType.radio),
+
   FieldSpec(
       name: "radio3",
       group: "Pronoun",
-      value: "They",
+      value: "Unspecified",
       type: FieldType.radio),
-      
-  FieldSpec(name: "checkbox", value: "checked", type: FieldType.checkbox)
+            
+  FieldSpec(
+    name: "checkbox", 
+    value: "checked", 
+    type: FieldType.checkbox)
 ];
 
 /// We are going to send the results here
--- lib/src/form_helper.dart
+++ lib/src/form_helper.dart
@@ -102,20 +102,6 @@ class FormHelper extends ChangeNotifier {
   /// All the values
   final Map<String, String> values = {};
 
-  /// Gets a field spec
-  FieldSpec _getFieldSpec(String name) =>
-      fields.firstWhere((element) => element.name == name);
-
-  /// Gets a field spec
-  int _getFieldSpecIndex(FieldSpec fieldSpec) => fields.indexOf(fieldSpec);
-
-  /// Get a focus node for a named field
-  FocusNode _getFocusNode(String name) => focusNodes[name];
-
-  /// Get a text editting controller for a name
-  TextEditingController _getTextEditingController(String name) =>
-      controllers[name];
-
   /// A count of validation errors
   int get validationErrors => fields.fold(
       0,
@@ -144,26 +130,6 @@ class FormHelper extends ChangeNotifier {
     return "Submit Form";
   }
 
-  /// Focus on the first remaining mandatory field
-  /// Used when user taps "submit" without completing the form
-  void _focusOnFirstRemaining() {
-    final field = fields.firstWhere(
-        (field) => field.mandatory && _getValue(field.name).isEmpty);
-    if (field != null) {
-      _getFocusNode(field.name).requestFocus();
-    }
-  }
-
-  /// Focus on the first error in the form
-  /// Used when user taps "submit" with errors detected in input
-  void _focusOnFirstError() {
-    final field = fields.firstWhere((field) =>
-        compositeValidator(field.validators, _getValue(field.name)) != null);
-    if (field != null) {
-      _getFocusNode(field.name).requestFocus();
-    }
-  }
-
   /// Dispose this form
   @override
   void dispose() {
@@ -173,33 +139,28 @@ class FormHelper extends ChangeNotifier {
     super.dispose();
   }
 
-  /// Called every time a value is changed
-  void _onChange(String name, String value) {
-    values[name] = value;
-    if (onChanged != null) {
-      /// Todo allow errors
-      onChanged(values);
-    }
-    notifyListeners();
-  }
-
   /// Build the form
   Widget buildForm({FormUiBuilder builder = scrollableSimpleForm}) =>
       builder(this);
 
-  void _onSubmit(String name) {
-    final spec = _getFieldSpec(name);
-    final idx = _getFieldSpecIndex(spec);
-    if (spec.type == FieldType.text) {
-      values[name] = _getTextEditingController(name).text;
+  /// Get's the Widget for a name
+  /// "submit" is a special case
+  Widget getWidget(String name) {
+    if (name == "submit") {
+      return RaisedButton(
+          onPressed: submitForm, child: Text(submissionButtonText));
     }
 
-    if (idx + 1 < fields.length) {
-      final nextField = fields[idx + 1];
-      _getFocusNode(nextField.name).requestFocus();
-    } else {
-      _getFocusNode(name).unfocus();
+    switch (_getFieldSpec(name).type) {
+      case FieldType.text:
+        return _TextField._(formHelper: this, name: name);
+      case FieldType.radio:
+        return _RadioButton(formHelper: this, name: name);
+      case FieldType.checkbox:
+        return _CheckBox(formHelper: this, name: name);
     }
+
+    return const Text("Uknown Type");
   }
 
   /// Call this when you want to "Submit" the form
@@ -222,22 +183,77 @@ class FormHelper extends ChangeNotifier {
     notifyListeners();
   }
 
-  /// Gets the current value for a field
-  /// Maybe not needed anymore
+  /// Focus on the first remaining mandatory field
+  /// Used when user taps "submit" without completing the form
+  void _focusOnFirstRemaining() {
+    final field = fields.firstWhere(
+        (field) => field.mandatory && _getValue(field.name).isEmpty);
+    if (field != null) {
+      _getFocusNode(field.name).requestFocus();
+    }
+  }
+
+  /// Focus on the first error in the form
+  /// Used when user taps "submit" with errors detected in input
+  void _focusOnFirstError() {
+    final field = fields.firstWhere((field) =>
+        compositeValidator(field.validators, _getValue(field.name)) != null);
+    if (field != null) {
+      _getFocusNode(field.name).requestFocus();
+    }
+  }
+
+  /// Gets a field spec
+  FieldSpec _getFieldSpec(String name) =>
+      fields.firstWhere((element) => element.name == name);
+
+  /// Gets a field spec
+  int _getFieldSpecIndex(FieldSpec fieldSpec) => fields.indexOf(fieldSpec);
+
+  /// Get a focus node for a named field
+  FocusNode _getFocusNode(String name) => focusNodes[name];
+
+  /// Get a text editting controller for a name
+  TextEditingController _getTextEditingController(String name) =>
+      controllers[name];
+
+  /// Called every time a value is changed
+  void _onChange(String name, String value) {
+    values[name] = value;
+    if (onChanged != null) {
+      /// Todo allow errors
+      onChanged(values);
+    }
+    notifyListeners();
+  }
+
+  void _onSubmit(String name) {
+    final spec = _getFieldSpec(name);
+    final idx = _getFieldSpecIndex(spec);
+    if (spec.type == FieldType.text) {
+      values[name] = _getTextEditingController(name).text;
+    }
+
+    if (idx + 1 < fields.length) {
+      final nextField = fields[idx + 1];
+      _getFocusNode(nextField.name).requestFocus();
+    } else {
+      _getFocusNode(name).unfocus();
+    }
+  }
+
+  /// Gets the current value for a field by name
+  /// Radio buttons get value by Group name
+  /// and default to a value = to the first option listed
   String _getValue(String name) {
     final field = _getFieldSpec(name);
-    switch (field.type) {
-      case FieldType.text:
-        return _getTextEditingController(name).text;
-      case FieldType.radio:
-        if (!values.containsKey(field.group)) {
-          return _getRadioDefaultValue(field.group);
-        }
-        return values[field.group];
-      case FieldType.checkbox:
-        return "";
+    if (field.type == FieldType.radio) {
+      if (!values.containsKey(field.group)) {
+        return _getRadioDefaultValue(field.group);
+      }
+      return values[field.group];
     }
-    return "";
+    return values[field.name];
   }
 
   String _getRadioDefaultValue(String group) =>
@@ -256,26 +272,6 @@ class FormHelper extends ChangeNotifier {
   }
 
   bool _isChecked(String name) => values.containsKey(name);
-
-  /// Get's the Widget for a name
-  /// "submit" is a special case
-  Widget getWidget(String name) {
-    if (name == "submit") {
-      return RaisedButton(
-          onPressed: submitForm, child: Text(submissionButtonText));
-    }
-
-    switch (_getFieldSpec(name).type) {
-      case FieldType.text:
-        return _TextField._(formHelper: this, name: name);
-      case FieldType.radio:
-        return _RadioButton(formHelper: this, name: name);
-      case FieldType.checkbox:
-        return _CheckBox(formHelper: this, name: name);
-    }
-
-    return const Text("Uknown Type");
-  }
 }
 
 /// Field Bindings for the helper
